dc6d966d38fce29e3db35b2e14dc1c3337cba93a
Thiago Franceschini <thiagoconsultor1@gmail.com>
2018-10-31 21:31:28 -0300

    crud completo

--- src/main/java/br/com/thiago/mongodb/controller/UserController.java
+++ src/main/java/br/com/thiago/mongodb/controller/UserController.java
@@ -1,16 +1,23 @@
 package br.com.thiago.mongodb.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.thiago.mongodb.domain.User;
 import br.com.thiago.mongodb.dto.UserDTO;
@@ -28,7 +35,7 @@ public class UserController {
 		List<UserDTO> usersDto = users.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(usersDto);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> findById(@PathVariable String id) {
 		if (!StringUtils.isEmpty(id)) {
@@ -37,6 +44,40 @@ public class UserController {
 			return ResponseEntity.status(HttpStatus.OK).body(dto);
 		} else {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Identificador invalido:" + id);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<?> insert(@RequestBody UserDTO dto) {
+		if (!ObjectUtils.isEmpty(dto)) {
+			User user = service.insert(dto);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId())
+					.toUri();
+			return ResponseEntity.created(uri).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parâmetro Usuário Invalido");
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody UserDTO dto, @PathVariable String id) {
+		if (!ObjectUtils.isEmpty(dto)) {
+			User user = service.fromDto(dto);
+			user.setId(id);
+			user = service.update(user);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parâmetro Usuário Invalido");
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable String id) {
+		if (!ObjectUtils.isEmpty(id)) {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parametro Usuário Invalido");
 		}
 	}
 
--- src/main/java/br/com/thiago/mongodb/services/UserService.java
+++ src/main/java/br/com/thiago/mongodb/services/UserService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import br.com.thiago.mongodb.domain.User;
+import br.com.thiago.mongodb.dto.UserDTO;
 import br.com.thiago.mongodb.repositories.UserRepository;
 import br.com.thiago.mongodb.service.exception.ObjectNotFoundException;
 
@@ -19,9 +21,44 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> optional = repository.findById(id);
-		return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: "+id));
+		return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: " + id));
 	}
+
+	public User insert(UserDTO dto) {
+		User savedUser = repository.save(fromDto(dto));
+		return savedUser;
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User update(User usuario) {
+		Optional<User> optional = repository.findById(usuario.getId());
+		User newUser = optional.get();
+		updateData(newUser, usuario);
+		return repository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
+	public User fromDto(UserDTO dto) {
+		User user = new User();
+		if (!StringUtils.isEmpty(dto.getName())) {
+			user.setName(dto.getName());
+		}
+		if (!StringUtils.isEmpty(dto.getEmail())) {
+			user.setEmail(dto.getEmail());
+		}
+		return user;
+	}
+	
+	
 }
